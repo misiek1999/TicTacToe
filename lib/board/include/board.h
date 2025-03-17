f673741060b37a6598cce23d51e65bca8388aaf6
misiek1999 <michaldydo99@gmail.com>
2025-03-17 23:35:28 +0100

    Add BoardLib with CMake configuration and board implementation

---
+++ lib/board/include/board.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <array>
+#include <expected>
+#include <memory>
+
+#include "player_type.h"
+
+namespace Board {
+    // Size of the board
+    constexpr size_t kBoardSize = 3U;
+
+    // Types of fields in the board
+    enum class BoardField : uint8_t {
+        EMPTY = 0,
+        X = 1,
+        O = 2
+    };
+
+    // Convert player type to board field
+    constexpr BoardField convertPlayerTypeToBoardField(PlayerType player) {
+        auto boardField = BoardField::EMPTY;
+        switch (player) {
+        case PlayerType::X:
+            boardField = BoardField::X;
+            break;
+        case PlayerType::O:
+            boardField = BoardField::O;
+            break;
+        default:
+            break;
+        }
+        return boardField;
+    }
+
+    // Type of the board
+    using BoardType = std::array<std::array<BoardField, kBoardSize>, kBoardSize>;
+
+    enum class BoardError {
+        INVALID_MOVE,
+        INVALID_PLAYER,
+        INVALID_STATE,
+        INVALID_COORDINATES,
+        NONE
+    };
+
+    class IBoard {
+    public:
+        virtual ~IBoard() = default;
+        virtual BoardType get_board() const = 0;
+        virtual bool is_full() const = 0;
+        virtual bool is_winner(PlayerType player) const = 0;
+        virtual bool is_valid_move(int row, int col) const = 0;
+        virtual std::expected<bool, BoardError> make_move(int row, int col, PlayerType player) = 0;
+        virtual void reset() = 0;
+
+    };
+
+    // Forward declaration
+    class BoardImpl;
+
+    // Board class
+    class Board{
+    public:
+        Board();
+        ~Board() = default;
+
+        BoardType get_board() const {
+            return board_impl_->get_board();
+        }
+
+        bool is_full() const {
+            return board_impl_->is_full();
+        }
+
+        bool is_winner(PlayerType player) const {
+            return board_impl_->is_winner(player);
+        }
+
+        bool is_valid_move(int row, int col) const {
+            return board_impl_->is_valid_move(row, col);
+        }
+
+        std::expected<bool, BoardError> make_move(int row, int col, PlayerType player) {
+            return board_impl_->make_move(row, col, player);
+        }
+
+        void reset() {
+            board_impl_->reset();
+        }
+
+    private:
+        std::unique_ptr<IBoard> board_impl_;
+    };
+}
---
+++ lib/board/src/board.cpp
@@ -0,0 +1,77 @@
+#include "board.h"
+#include <ranges>
+#include <algorithm>
+
+namespace Board {
+
+class BoardImpl : public IBoard{
+public:
+    BoardImpl() {
+        this->reset();
+    }
+
+    ~BoardImpl() = default;
+
+    BoardType get_board() const override{
+        return board_;
+    }
+
+    bool is_full() const override {
+        return not std::ranges::any_of(board_, [](const auto& row) {
+            return std::ranges::any_of(row, [](const auto& field) {
+                return field == BoardField::EMPTY;
+            });
+        });
+    }
+
+    bool is_winner(PlayerType player) const override {
+        const auto board_player = convertPlayerTypeToBoardField(player);
+        for (size_t i = 0; i < kBoardSize; ++i) {
+            if (board_[i][0] == board_player && board_[i][1] == board_player && board_[i][2] == board_player) {
+                return true;
+            }
+            if (board_[0][i] == board_player && board_[1][i] == board_player && board_[2][i] == board_player) {
+                return true;
+            }
+        }
+        if (board_[0][0] == board_player && board_[1][1] == board_player && board_[2][2] == board_player) {
+            return true;
+        }
+        if (board_[0][2] == board_player && board_[1][1] == board_player && board_[2][0] == board_player) {
+            return true;
+        }
+        return false;
+    }
+
+    bool is_valid_move(int row, int col) const override {
+        if (row < 0 || row >= static_cast<int>(kBoardSize) || col < 0 || col >= static_cast<int>(kBoardSize)) {
+            return false;
+        }
+        return board_[row][col] == BoardField::EMPTY;
+    }
+
+    std::expected<bool, BoardError> make_move(int row, int col, PlayerType player) override {
+        if (!is_valid_move(row, col)) {
+            return std::unexpected(BoardError::INVALID_MOVE);
+        }
+        const auto board_player = convertPlayerTypeToBoardField(player);
+        if (board_player != BoardField::EMPTY) {
+            return std::unexpected(BoardError::INVALID_PLAYER);
+        }
+        board_[row][col] = board_player;
+        return true;
+    }
+
+    void reset() override {
+        auto flat_view = board_ | std::views::join;
+        std::ranges::fill(flat_view, BoardField::EMPTY);
+    }
+private:
+    BoardType board_;
+};
+
+// This constructor needs to be defined in the .cpp file after Implementation of the BoardImpl class, due to the unique_ptr initizalization
+Board::Board() : board_impl_(std::make_unique<BoardImpl>()) {
+}
+
+} // namespace Board
