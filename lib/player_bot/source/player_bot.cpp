63f55b36dff553dea3f917250dbc50ea1c484391
misiek1999 <michaldydo99@gmail.com>
2025-03-26 00:18:53 +0100

    Fix player bot implementation class name

--- lib/player_bot/source/player_bot.cpp
+++ lib/player_bot/source/player_bot.cpp
@@ -10,15 +10,17 @@ namespace Player
 constexpr size_t kMaxGeneratedNumber= 2U;
 constexpr size_t kMinGeneratedNumber= 0U;
 
-class PlayerBot : public IPlayer
+class PlayerBotImpl : public IPlayer
 {
 public:
-    explicit PlayerBot(BoardPlayerType player_type) :
+    explicit PlayerBotImpl(BoardPlayerType player_type) :
             IPlayer(player_type),
             gen_(rd_()) {
+        distrib = std::uniform_int_distribution<>(kMinGeneratedNumber, kMaxGeneratedNumber);
     }
 
     std::pair<int, int> get_move(const Board::Board &board) override {
+        std::ignore = board;
         return {distrib(gen_), distrib(gen_)};
     };
 
@@ -26,9 +28,12 @@ private:
         // Create a random device and a Mersenne Twister generator seeded with it
         std::random_device rd_;
         std::mt19937 gen_;
-        std::uniform_int_distribution<> distrib = {kMinGeneratedNumber, kMaxGeneratedNumber};
+        std::uniform_int_distribution<> distrib;
 };
 
-PlayerBot::PlayerBot(BoardPlayerType player_type) : impl_(std::make_unique<PlayerBotImpl>(player_type)) {}
+PlayerBot::PlayerBot(BoardPlayerType player_type):
+    IPlayer(player_type),
+    impl_(std::make_unique<PlayerBotImpl>(player_type)) {
+}
 
 } // namespace Player
