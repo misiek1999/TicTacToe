c96daef25d7561323d9c40671eb83e3cab97df93
misiek1999 <michaldydo99@gmail.com>
2025-05-08 18:41:58 +0200

    Change log level to error

--- lib/log/log.cpp
+++ lib/log/log.cpp
@@ -1,7 +1,8 @@
 #include "log.h"
 
 void init_logger() {
-    spdlog::set_level(spdlog::level::trace);
+    // spdlog::set_level(spdlog::level::trace);
+    spdlog::set_level(spdlog::level::err); // Set global log level to error
     spdlog::set_pattern("[%H:%M:%S.%e][%L][tid: %t] %v");
     spdlog::info("Logger initialized");
 }
