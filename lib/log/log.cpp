b2f36e4bd3abde40d2c869f77b71eab3540c0e9f
misiek1999 <michaldydo99@gmail.com>
2025-05-14 00:21:42 +0200

    Add function name and line number to spdlog

--- lib/log/log.cpp
+++ lib/log/log.cpp
@@ -3,6 +3,6 @@
 void init_logger() {
     // spdlog::set_level(spdlog::level::trace);
     spdlog::set_level(spdlog::level::err); // Set global log level to error
-    spdlog::set_pattern("[%H:%M:%S.%e][%L][tid: %t] %v");
+    spdlog::set_pattern("[%H:%M:%S.%e][%L][tid: %t]%s:%# %v");
     spdlog::info("Logger initialized");
 }
--- lib/log/log.h
+++ lib/log/log.h
@@ -4,12 +4,12 @@
 
 
 // Logging macros
-#define LOG_V(...) spdlog::trace(__VA_ARGS__)
-#define LOG_D(...) spdlog::debug(__VA_ARGS__)
-#define LOG_I(...) spdlog::info(__VA_ARGS__)
-#define LOG_W(...) spdlog::warn(__VA_ARGS__)
-#define LOG_E(...) spdlog::error(__VA_ARGS__)
-#define LOG_F(...) spdlog::critical(__VA_ARGS__)
+#define LOG_V(...) SPDLOG_LOGGER_TRACE(spdlog::default_logger_raw(), __VA_ARGS__)
+#define LOG_D(...) SPDLOG_LOGGER_DEBUG(spdlog::default_logger_raw(), __VA_ARGS__)
+#define LOG_I(...) SPDLOG_LOGGER_INFO(spdlog::default_logger_raw(), __VA_ARGS__)
+#define LOG_W(...) SPDLOG_LOGGER_WARN(spdlog::default_logger_raw(), __VA_ARGS__)
+#define LOG_E(...) SPDLOG_LOGGER_ERROR(spdlog::default_logger_raw(), __VA_ARGS__)
+#define LOG_F(...) SPDLOG_LOGGER_CRITICAL(spdlog::default_logger_raw(), __VA_ARGS__)
 
 // Initialize logger
 void init_logger();
